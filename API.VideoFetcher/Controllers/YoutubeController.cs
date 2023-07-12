d6a322f7d9f8ccbf38ad9784e2e2d684d95efd55
xstefano <xcloudsgls@gmail.com>
2023-07-12 13:22:30 -0600

    implementing YoutubeController

---
+++ API.VideoFetcher/Controllers/YoutubeController.cs
@@ -0,0 +1,21 @@
+﻿using API.VideoFetcher.Interfaces;
+using API.VideoFetcher.Message;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.VideoFetcher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YoutubeController : ControllerBase
+    {
+        private readonly IYoutubeService _youtubeService;
+        protected Response _response;
+
+        public YoutubeController(IYoutubeService youtubeService)
+        {
+            _youtubeService = youtubeService;
+            _response = new Response();
+        }
+    }
+}
