850b9ba9532162f1760ea9b11384bd63fac8b447
xstefano <xcloudsgls@gmail.com>
2023-07-12 12:31:55 -0600

    implementing the IYoutubeService class

---
+++ API.VideoFetcher/Interfaces/IYoutubeService.cs
@@ -0,0 +1,15 @@
+﻿using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+
+namespace API.VideoFetcher.Interfaces
+{
+    public interface IYoutubeService
+    {
+        Task<Video> GetVideo(string videoUrl);
+        Task<string> GetBestUrlVideo(string videoUrl);
+        Task<IEnumerable<MuxedStreamInfo>> GetContainerMp4(string videoUrl);
+        Task<IEnumerable<AudioOnlyStreamInfo>> GetCointanerM4a(string videoUrl);
+        Task<Stream> DownloadMp4(string videoUrl, string url);
+        Task<Stream> DownloadM4a(string videoUrl, int bytes);
+    }
+}
